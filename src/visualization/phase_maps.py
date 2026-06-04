ae59f435d8897a0bd455a64f41583db1ce53a51e
andjamilloshi1-cmd <andjamilloshi1@gmail.com>
2026-06-04 02:57:43 +0200

    Update phase_maps.py

--- src/visualization/phase_maps.py
+++ src/visualization/phase_maps.py
@@ -1,1 +1,8 @@
-
+import matplotlib.pyplot as plt
+def plot_phase_space(x_list, y_list, title):
+    # Vizaton grafikun e shperndarjes
+    plt.figure(figsize=(8, 6))
+    plt.scatter(x_list, y_list, s=0.1, color='blue')
+    plt.title(title)
+    plt.grid(True)
+    plt.show()
