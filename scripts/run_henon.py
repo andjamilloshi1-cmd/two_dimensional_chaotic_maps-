084b306b46081d8fedc318c54b9c66950d566d67
andjamilloshi1-cmd <andjamilloshi1@gmail.com>
2026-06-04 02:50:13 +0200

    Update run_henon.py

--- scripts/run_henon.py
+++ scripts/run_henon.py
@@ -1,28 +1,16 @@
-import sys import os
-# Shtojme rrugen e projektit per te importuar
-modulet nga dosja src
-sys. path. appena(os •path. abspath (os.path. join(os
-•path dirname(_file), ' •.')))
-from src.models.henon import henon mar from src.visualization phase_maps import
-plot_phase_space
-def run ():
-# Parametrat fillestare per simulimin
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(*file*), '..')))
+from src.models.henon import henon_map
+from src.visualization.phase_maps import plot_phase_space
+def run():
 x, y = 0.1, 0.1
-*_lista
-=[]
-Y_lista = []
-# Simulimi per 10,000 hapa per te
-formuar atraktorin
-print ( "Duke llogaritur harten e
-Henonit……"'）
-for i in range (10000) :
-x, y = henon_map (x, y)
-x lista.append (x)
-y_ lista. append (y)
-# Gjenerimi i grafikut
-plot phase space(x_lista, y_lista,
-"Atraktori i Henonit")
-print ( "Simulimi perfundoi me sukses.")
-if
-name run ()
-main ":
+x_lista = []
+y_lista = []
+for i in range(10000):
+x, y = henon_map(x, y)
+x_lista.append(x)
+y_lista.append(y)
+plot_phase_space(x_lista, y_lista, "Atraktori i Henonit")
+if *name* == "*main*":
+run()
